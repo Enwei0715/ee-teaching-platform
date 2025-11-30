dfecd5520c38aa68a2f9f616a90d718b8b2fe79b
Lorry <enweiliu0715@gmail.com>
2025-11-30 14:59:08 +0800

    Fix build errors: Remove assignments route and update MDXContent

--- src/components/mdx/MDXContent.tsx
+++ src/components/mdx/MDXContent.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
-import Quiz from '@/components/course/Quiz';
 import { mdxComponents } from './mdx-components';
 
 interface Props {
@@ -13,13 +12,6 @@ interface Props {
 export default function MDXContent({ source, courseId, lessonId }: Props) {
     const componentsWithContext = {
         ...mdxComponents,
-        Quiz: (props: any) => (
-            <Quiz
-                {...props}
-                courseId={courseId || props.courseId}
-                lessonId={lessonId || props.lessonId}
-            />
-        ),
     };
 
     return <MDXRemote {...source} components={componentsWithContext} />;
